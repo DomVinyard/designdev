2a4a9a94c8b8006e719ec5d3dc68120cd3076764
Dom Vinyard <domvinyard@me.com>
2019-08-12 15:41:24 +0100

    build in env

--- functions/webhookProxy.js
+++ functions/webhookProxy.js
@@ -1,14 +1,14 @@
+require("dotenv").config()
 const axios = require("axios")
-const BUILD = "https://api.netlify.com/build_hooks/5d517857c295bc8c9d0a8bca"
 exports.handler = async function(event, context, callback) {
+  const { NETLIFY_BUILD_HOOK } = process.env
   const challenge = event.queryStringParameters.challenge
-  console.log({ challenge })
 
   // TODO return if no actionable changes
   // get latest build date
   // filter publishable files (rocket no edit)
   // check if any latest file date is after latest build
 
-  await axios.post(BUILD)
-  callback(null, { statusCode: 200 })
+  await axios.post(NETLIFY_BUILD_HOOK)
+  callback(null, { statusCode: 200, body: challenge })
 }
