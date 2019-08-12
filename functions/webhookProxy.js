78eaba70868ecf1c6c209d0357341153b98564c4
Dom Vinyard <domvinyard@me.com>
2019-08-12 15:36:15 +0100

    post

--- functions/webhookProxy.js
+++ functions/webhookProxy.js
@@ -1,12 +1,14 @@
-exports.handler = function(event, context, callback) {
+const axios = require("axios")
+const BUILD = "https://api.netlify.com/build_hooks/5d517857c295bc8c9d0a8bca"
+exports.handler = async function(event, context, callback) {
   const challenge = event.queryStringParameters.challenge
   console.log({ challenge })
-  callback(null, {
-    headers: {
-      "Content-Type": "text/plain",
-      "X-Content-Type-Options": "nosniff",
-    },
-    statusCode: 200,
-    body: challenge,
-  })
+
+  // TODO return if no actionable changes
+  // get latest build date
+  // filter publishable files (rocket no edit)
+  // check if any latest file date is after latest build
+
+  await axios.post(BUILD)
+  callback(null, { statusCode: 200 })
 }
