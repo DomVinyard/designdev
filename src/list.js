e92029b724580d5f31eaa3963d20e79f85598aaa
Dom Vinyard <domvinyard@me.com>
2019-08-13 23:26:40 +0100

    reverse list

--- src/list.js
+++ src/list.js
@@ -3,7 +3,28 @@ import { Link } from "gatsby"
 import { Helmet } from "react-helmet"
 import ReactMarkdown from "react-markdown"
 
+const PageLink = ({ date, content, nav: { isFirst, isLatest } }) => (
+  <div>
+    <Link to={date}>
+      <label>{isFirst ? "start" : date}</label>
+      <span>
+        <ReactMarkdown
+          source={content
+            .split(`\n`)
+            .filter(Boolean)[0]
+            .replace(/([.?!,:])\s*(?=[A-Z])/g, "$1|")
+            .split("|")[0]
+            .slice(0, -1)}
+        />
+      </span>
+    </Link>
+  </div>
+)
+
 const List = ({ pageContext: { pages } }) => {
+  const first = pages.slice(-1)[0]
+  const latest = pages[0]
+  const pastWeek = pages.slice(1, 7)
   return (
     <main>
       <Helmet>
@@ -14,26 +35,23 @@ const List = ({ pageContext: { pages } }) => {
       <header>
         <h1>🚀dom.fyi</h1>
       </header>
-      <article>A daily update about Design & Dev.</article>
-      <nav>
-        {pages.map(({ date, content, nav: { isFirst, isLatest } }) => (
-          <div>
-            <Link to={date}>
-              <label>{isFirst ? "start" : date}</label>
-              <span>
-                <ReactMarkdown
-                  source={content
-                    .split(`\n`)
-                    .filter(Boolean)[0]
-                    .replace(/([.?!,:])\s*(?=[A-Z])/g, "$1|")
-                    .split("|")[0]
-                    .slice(0, -1)}
-                />
-              </span>
-            </Link>
-          </div>
-        ))}
-      </nav>
+      <article>
+        <p>A daily update about Design & Dev.</p>
+        <nav>
+          {pages.reverse().map(page => (
+            <PageLink {...page} />
+          ))}
+        </nav>
+        {false && (
+          <nav>
+            <PageLink {...first} />
+            {pastWeek.reverse().map(page => (
+              <PageLink {...page} />
+            ))}
+            <PageLink {...latest} />
+          </nav>
+        )}
+      </article>
     </main>
   )
 }
