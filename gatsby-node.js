9d29d58345d6978d9235ca419480d0f9418d2105
Dom Vinyard <domvinyard@me.com>
2019-08-19 23:28:26 +0100

    filter

--- gatsby-node.js
+++ gatsby-node.js
@@ -3,10 +3,8 @@ const { resolve } = require("path")
 const { graphql } = require("gatsby")
 const isDevMode = process.env.NODE_ENV === "development"
 
-// dom.fyi
-
+// dom.fyi. get all notes from dropbox
 exports.createPages = async ({ graphql, actions }) => {
-  // Get all notes from dropbox
   const { data } = await graphql(`
     # Get all notes (date, html, excerpt)
     query GetNotes {
@@ -23,38 +21,32 @@ exports.createPages = async ({ graphql, actions }) => {
       }
     }
   `)
-  const first = data.dropbox.notes[0]
-
-  // make a page for each markdown file
-  data.dropbox.notes.forEach(({ note }, i) => {
-    if (note && note.date && note.content) {
-      const { html, excerpt } = note.content
-      if (!excerpt || !html) return false // if has content
-      if (!/^\d{4}\.\d{1,3}$/.test(note.date)) return false // and is valid date
-      if (!isDevMode && !excerpt.trim().startsWith("🚀")) return false // and is published
-      const next = data.dropbox.notes[i + 1]
-      actions.createPage({
-        path: note.date,
-        component: resolve(`./src/note.js`),
-        context: {
-          date: note.date,
-          next: next && `/${next.note.date}`,
-          first: i > 0 && `/${first.note.date}`,
-        },
-      })
-    }
+  const notes = data.dropbox.notes
+    .filter(({ note }) => note && note.date && note.content) // build every .md file
+    .filter(({ note: { content } }) => content.excerpt && content.html) // with content
+    .filter(({ note: { date } }) => /^\d{4}\.\d{1,3}$/.test(date)) // a valid date
+    .filter(({ note: { content } }) => content.excerpt.trim().startsWith("🚀")) // and published
+    .map(({ note }) => note)
+  notes.forEach((note, i) => {
+    actions.createPage({
+      path: note.date,
+      component: resolve(`./src/note.js`),
+      context: {
+        date: note.date,
+        next: notes[i + 1] && `/${notes[i + 1].date}`,
+        first: i > 0 && `/${notes[0].date}`,
+      },
+    })
   })
 
-  // list page
+  // list page and home page
   await actions.createPage({
     path: `/list`,
     component: resolve(`./src/list.js`),
   })
-
-  // home page
   return await actions.createRedirect({
     fromPath: "/",
-    toPath: `/${first.note.date}`,
+    toPath: `/${notes[notes.length - 1].date}`,
     redirectInBrowser: true,
     statusCode: 200,
   })
