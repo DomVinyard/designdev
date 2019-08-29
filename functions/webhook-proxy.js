956e1e7849bfaa8223d13a3623200a97dc09fa3d
dom <domvinyard@me.com>
2019-08-30 00:59:31 +0100

    challenge

--- functions/webhook-proxy.js
+++ functions/webhook-proxy.js
@@ -1,11 +1,11 @@
 require("dotenv").config()
 const axios = require("axios")
 exports.handler = async function(event, context, callback) {
+  const challenge = event.queryStringParameters.challenge
+  if (challenge) return callback(null, { statusCode: 200, body: challenge })
   const { NETLIFY_BUILD_HOOK } = process.env
   if (!NETLIFY_BUILD_HOOK) throw "where is prod?"
-  const challenge = event.queryStringParameters.challenge
   await axios.post(NETLIFY_BUILD_HOOK)
-  callback(null, { statusCode: 200, body: challenge })
 }
 
 // require("dotenv").config()
