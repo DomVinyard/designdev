4d6c9e807e772c4656050bc5bc43950bbb641905
dom <domvinyard@me.com>
2019-08-30 01:09:53 +0100

    yearday teaks

--- src/yearday.js
+++ src/yearday.js
@@ -1,6 +1,6 @@
 
 import moment from 'moment'
-export default (arg, options) => {
+export default (arg, options = {}) => {
     if (typeof arg === "string") {
       const valid = /^\d{4}\.\d{1,3}$/.test(arg)
       if (!valid) return new Error("invalid string")
@@ -8,4 +8,13 @@ export default (arg, options) => {
       const ISO8601 = `${year}-${day.padStart(3, "0")}`
       return moment(ISO8601)
     }
+    if (typeof arg === 'date') {
+      arg.toString = ({time}) => 'xxxx.xxx'
+      arg.isValid = false
+    }
   }
+
+// YearDay(string) // to date (with properties)
+// YearDay(string).isValid // unless native invalid date
+// YearDay(date).toString({ time: false }) // default true if present 
+// YearDay(date, { world: true })
