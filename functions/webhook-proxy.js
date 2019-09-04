7afab766a25e5fd78225e62a7e4b6f4125f0e05e
dom <domvinyard@me.com>
2019-09-04 19:00:16 +0100

    restore challenge

--- functions/webhook-proxy.js
+++ functions/webhook-proxy.js
@@ -2,14 +2,12 @@
     Every time Dropbox updates (because a note was added or edited),
     Dropbox will fire a webhook to here 👇📝👇.
 
-    Requires .env variable `NETLIFY_BUILD_HOOK`
+    Requires .env variable `NETLIFY_BUILD_HOOK` (see /gatsby-node)
 */
 
 require("dotenv").config()
 if (!process.env.NETLIFY_BUILD_HOOK) throw "where is prod?"
-exports.handler = () => require("axios").post(process.env.NETLIFY_BUILD_HOOK)
-
-/*
-    The `NETLIFY_BUILD_HOOK` env variable contains an endpoint 
-    which will triggers all pages to build and deploy (see /gatsby-node). 
-*/
+exports.handler = ({ queryStringParameters: challenge }) => {
+  if (challenge) return callback(null, { statusCode: 200, body: challenge }) // confirm webhook location with dropbox
+  require("axios").post(process.env.NETLIFY_BUILD_HOOK)
+}
