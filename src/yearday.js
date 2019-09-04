aa6f3c85138a185a55277c7b974a186f264cf067
dom <domvinyard@me.com>
2019-09-04 16:24:09 +0100

    yearday

--- src/yearday.js
+++ src/yearday.js
@@ -2,31 +2,31 @@
   Parser for YearDay format dates
   Docs at https://dom.fyi/2019.220
 
-  Returns a YearDate object, which is a moment object with 
-  additional properties.
+  Returns a YearDate object, which is actually just a momentjs object
 */
 
-import moment from "moment"
+import moment from "moment" // https://momentjs.com
+
+// string to date
 export default (arg, options = {}) => {
   if (typeof arg === "string") {
+    // TODO: time
     const valid = /^\d{4}\.\d{1,3}$/.test(arg)
-    if (!valid) return new Error("invalid string")
+    if (!valid) return new Error("invalid date")
     const [year, day] = arg.split(".")
-    const ISO8601 = `${year}-${day.padStart(3, "0")}`
-    if (options.world)
-      // TODO: add 10,000 years to the date.
-      // (https://en.wikipedia.org/wiki/Holocene_calendar)
-      return false
+    const paddedDay = day.padStart(3, "0")
+    const ISO8601 = [year, paddedDay].join("-")
+    if (options.world) return moment(ISO8601).add("years", 10000) // https://en.wikipedia.org/wiki/Holocene_calendar
     return moment(ISO8601)
   }
-  if (typeof arg === "date") {
+
+  // TODO: date to string
+  if (arg instanceof Date) {
     const momentObject = moment(arg)
     momentObject.toString = ({ time }) => {
-      // TODO: convert js date into YearDay string.
       if (!time) return "xxxx.xxx"
       return "xxxx.xxx.xxxxxxx"
     }
-    momentObject.isValid = false // TODO: determine if date valid
     return momentObject
   }
 }
