a7ea9c3179eaea0f9bb5d397333cfe8a4b4f23a0
Dom <domvinyard@me.com>
2019-10-29 15:36:05 +0000

    Update gatsby-node.js

--- gatsby-node.js
+++ gatsby-node.js
@@ -65,7 +65,7 @@ exports.createPages = async ({ graphql, actions }) => {
   await actions.createRedirect({
     fromPath: "https://yearday.org",
     toPath: "https://dom.fyi/2019.220",
-    redirectInBrowser: true,
+    redirectInBrowser: false,
     statusCode: 200,
   })
   return await actions.createRedirect({
